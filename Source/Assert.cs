85d7e188f44cb8dd7028476611afc6b4a4e50bc2
Stefnotch <brandmairstefan@gmail.com>
2019-11-03 21:30:49 +0100

    Improve Asserts

--- Source/Assert.cs
+++ Source/Assert.cs
@@ -13,9 +13,8 @@ namespace FlaxEngine.UnitTesting
         public static void Pass() => throw new SuccessException();
         public static void Fail() => throw new Exception();
 
-        // TODO: use Equals instead of ==
-        public static void AreEqual(object a, object b) { if (a != b) throw new Exception(); }
-        public static void AreNotEqual(object a, object b) { if (a == b) throw new Exception(); }
+        public static void AreEqual(object a, object b) { if (!Equals(a, b)) throw new Exception(); }
+        public static void AreNotEqual(object a, object b) { if (Equals(a, b)) throw new Exception(); }
 
         public static void True(bool a) { if (!a) throw new Exception(); }
         public static void False(bool a) { if (a) throw new Exception(); }
