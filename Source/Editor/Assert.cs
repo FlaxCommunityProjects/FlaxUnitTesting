b19a2445e94ac9770bc72682bbc48e0efc99e30a
Stefnotch <brandmairstefan@gmail.com>
2019-11-04 09:46:17 +0100

    Improve exception messages

--- Source/Editor/Assert.cs
+++ Source/Editor/Assert.cs
@@ -21,15 +21,34 @@ namespace FlaxCommunity.UnitTesting.Editor
         }
     }
 
+    /// <summary>
+    /// Exception thrown when an assertion fails
+    /// </summary>
+    /// <seealso cref="System.Exception" />
+    public class AssertException : Exception
+    {
+        public AssertException()
+        {
+        }
+
+        public AssertException(string message) : base(message)
+        {
+        }
+
+        public AssertException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
     public static class Assert
     {
         public static void Pass() => throw new SuccessException();
-        public static void Fail() => throw new Exception();
+        public static void Fail() => throw new AssertException("Fail");
 
-        public static void AreEqual(object a, object b) { if (!Equals(a, b)) throw new Exception(); }
-        public static void AreNotEqual(object a, object b) { if (Equals(a, b)) throw new Exception(); }
+        public static void AreEqual(object a, object b) { if (!Equals(a, b)) throw new AssertException($"{a} does not equal {b}"); }
+        public static void AreNotEqual(object a, object b) { if (Equals(a, b)) throw new Exception($"{a} is equal to {b}"); }
 
-        public static void True(bool a) { if (!a) throw new Exception(); }
-        public static void False(bool a) { if (a) throw new Exception(); }
+        public static void True(bool a) { if (!a) throw new Exception($"{a} is not true"); }
+        public static void False(bool a) { if (a) throw new Exception($"{a} is not false"); }
     }
 }
